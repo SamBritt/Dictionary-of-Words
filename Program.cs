67f9c594362175fd6f9f64cdbaff022ff34efe7a
SamBritt <sam.britt2@yahoo.com>
2019-05-28 15:16:38 -0500

    first commit using dictionaries

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace dictionary_of_words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> wordsWithDefs = new Dictionary<string, string>();
+
+            //Added a few words
+            wordsWithDefs.Add("happy", "feeling or showing pleasure or contentment.");
+            wordsWithDefs.Add("sad", "feeling or showing sorrow; unhappy.");
+            wordsWithDefs.Add("carrot", "a tapering orange-colored root eaten as a vegetable.");
+
+            Console.WriteLine(wordsWithDefs["carrot"]);
+            Console.WriteLine("------");
+            foreach(KeyValuePair<string, string> word in wordsWithDefs){
+                Console.WriteLine($"Word: {word.Key}, Definition: {word.Value}");
+            }
+
+            Console.WriteLine("<---Test--->");
+        }
+    }
+}
